39af286c63e066d22c6d181de66ed57cb8386353
asaltcode <elangovanr558@gmail.com>
2024-01-24 15:26:40 +0530

    updated

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 # Node JS File System Task
+
--- src/controller/TimeStamp.js
+++ src/controller/TimeStamp.js
@@ -11,14 +11,10 @@ const getTime = async (req, res) => {
     const content = `${date.toLocaleString()}`;
     await fs.writeFile(filePath, content);
     let datas = await fs.readFile(filePath, "utf8");
-    res.send(
-      // `<body style="display: grid; overflow:  hidden; place-items: center; height: 100vh;"><h1>${datas}</h1></body>`
-      {Date_Time : `${datas}`}
-    );
+    res.send({ message: "Fetched data successfully", Date_Time: datas });
   } catch (error) {
     res.send({
-      message ""Intrenal server Error""
-      message :
+      message: "Intrenal server Error",
     });
   }
 };
